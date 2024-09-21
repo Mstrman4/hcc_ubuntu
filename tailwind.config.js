a09ac69d147d7dba800fa50f74b6767cd6e5121e
Mstrman4 <daramade4@gmail.com>
2024-09-21 11:49:10 +0330

    1up

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", 
+    "./src/**/*.{html,js}",
+    "./js/**/*.js"  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
