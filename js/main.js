9c613d9f3720dcd020902c2244283a7a88080447
Mstrman4 <daramade4@gmail.com>
2024-09-25 17:39:09 +0330

    last

--- js/main.js
+++ js/main.js
@@ -10,22 +10,22 @@ import { sec4 } from "../component/sec4/sec4.js";
 
 
 
-// window.customElements.define(`nav-r`, navs);
-// window.customElements.define(`main-h`, mainHeader);
+window.customElements.define(`nav-r`, navs);
+window.customElements.define(`main-h`, mainHeader);
 window.customElements.define(`sec-two`, sec2);
 window.customElements.define(`sec-three`, sec3);
 window.customElements.define(`sec-for`, sec4);
 
-// let navm = document.createElement("nav-r")
-// let mh = document.createElement("main-h")
-// let secs2 = document.createElement("sec-two")
-// let secs3 = document.createElement("sec-three")
+let navm = document.createElement("nav-r")
+let mh = document.createElement("main-h")
+let secs2 = document.createElement("sec-two")
+let secs3 = document.createElement("sec-three")
 let secs4 = document.createElement("sec-for")
 
 
-// $.querySelector("header").appendChild(navm)
-// $.querySelector("header").appendChild(mh)
-// $.querySelector("#sec2").appendChild(secs2)
-// $.querySelector("#sec3").appendChild(secs3)
+$.querySelector("header").appendChild(navm)
+$.querySelector("header").appendChild(mh)
+$.querySelector("#sec2").appendChild(secs2)
+$.querySelector("#sec3").appendChild(secs3)
 $.querySelector(".bg-sec4").appendChild(secs4)
 $.querySelector(".bg-sec4").insertAdjacentHTML("beforeend",`<a href="#" class=" p-4">ccc</a>`)
