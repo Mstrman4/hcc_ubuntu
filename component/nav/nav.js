e2a2e64e7733bac25f2887836878b6a021078cdb
Mstrman4 <daramade4@gmail.com>
2024-10-07 02:23:45 +0330

    test-abs

--- component/nav/nav.js
+++ component/nav/nav.js
@@ -47,9 +47,7 @@ template.innerHTML = `
                   <li>
                     <a href="#sec8" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">تیم اجرایی</a>
                   </li>
-                  <li>
-                    <a href="#sec10" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">تایم لاین</a>
-                  </li>
+                
                   <li>
                     <a href="#sec9" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">ثبت نام </a>
                   </li>
