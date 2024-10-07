85d2ac8a7c7a694dd973a170879b683df4fa5b15
Mstrman4 <daramade4@gmail.com>
2024-10-07 20:42:13 +0330

    اخرین پوش:)

--- component/sec7/sec7.js
+++ component/sec7/sec7.js
@@ -2,10 +2,16 @@
 const template = document.createElement('template');
 
 let list =[
-  { link:"https://hmstp.ir/" , img:"img/park.png", title:"پارک علم و فناوری" , des:"پارک علم و فناوری استان هرمزگان به منظور ایجاد زیرساخت های مناسب در راستای توسعه علم و فناوری"},
+  { link:"https://bandarabbas.aut.ac.ir/" , img:"img/AMIRKABIR.png", title:"دانشگاه امیرکبیر " , des:"دانشگاه صنعتی امیر کبیر حوضه کشتی سازی"},
   { link:"https://limoo.host/" ,img:"img/limo.png", title:"لیمو هاست" , des:"از تنوع سرویس‌های میزبانی وب (web hosting) تا بهینه‌ سازی سایت و مهاجرت رایگان ما تمام راه‌های منحصر به فرد "},
-  // { link:"https://ramakdairy.com/", img:"img/ramak.png", title:"رامک" , des:"شرکت فرآورده های لبنی  تولیدکننده انواع محصولات لبنی و یکی از بزرگترین زیرمجموعه های هلدینگ دلتا در ایران است "},
+  { link:"https://www.instagram.com/zhstudiio" ,img:"img/zhin.png", title:"استدیو ژین" , des:"ارائه خدمات حرفه‌ای ادیت ویدیو، طراحی بنر و پوستر، و لوگوموشن؛ کمک می‌کنیم تا محتوای بصری خود را به شکلی خلاقانه و منحصر به فرد ارائه دهید."},
+  { link:"https://Arkadevelop.com" ,img:"img/arka.png", title:"ارکا develop" , des:"تیم آرکا توسعه هدف توسعه نرم افزارهای کارآمد و با کیفیت با استفاده از تکنولوژی های روز دنیا آغاز به کار نمود"},
+  { link:"https://www.instagram.com/metakish" ,img:"img/fulad.png", title:"فولاد سدید کیش " , des:" شرکت فولاد سدید سپهر کیش سال ها حضور خوشنام در بازار فولاد ایران  محکم تر و سخت تر از فولاد"},
+  { link:"https://mitadent.ir/" ,img:"img/dandon.png", title:" دندان پزشکی میتادنت" , des:"ما در تلاش هستیم تا با استفاده از کادر مجرب و کارآموزده خود و همچنین بهره گیری از بروزترین و امکانات و خدماتی درجه یک را به شما عزیزان ارائه دهیم"},
+  { link:"https://t.me/fax_vpn" ,img:"img/fax-vpn.png", title:"تحریم شکن فکس" , des:"ما همواره در تلاشیم اتصالی امن و بدون محدودیت برای شما عزیزان فراهم کنیم خدماتی شامل سرویس های مناسب (گیم , ترید , وبگردی و سایر موارد ) با بهترین سرعت و قیمت فراهم کنیم "},
+  
 ]
+//{ link:"https://ramakdairy.com/", img:"img/ramak.png", title:"رامک" , des:"شرکت فرآورده های لبنی  تولیدکننده انواع محصولات لبنی و یکی از بزرگترین زیرمجموعه های هلدینگ دلتا در ایران است "},
 
 template.innerHTML = `
 <link rel="stylesheet" href="../output.css">
@@ -32,7 +38,7 @@ class sec7 extends HTMLElement {
     
     list.forEach( (spons)=>{
       
-      let temp = `  <a href="${spons.link}"  class="p-4 w-full md:w-6/12 lg:w-4/12" >  <div>
+      let temp = `  <a href="${spons.link}"  class="p-4 w-full md:w-6/12 lg:w-4/12" >  <div class="h-full">
           <div class="bg-white flex p-4 rounded-lg w-full  gap-2 items-center h-full">
               <img src="${spons.img}" class="h-fit" alt="">
               <div  class="flex flex-col gap-3">
